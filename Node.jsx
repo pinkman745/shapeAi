1b5c5c4d7d9a494778bb27d9a144ce3effc874e8
pinkman745 <85457877+pinkman745@users.noreply.github.com>
2021-06-07 01:20:37 +0530

    Add files via upload

---
+++ Node.jsx
@@ -0,0 +1,15 @@
+  
+import React from "react";
+function Node(){
+return (
+   <div className = "note"> 
+     <h1>javascript and React.js</h1>
+     <p>this was an amazing boot camp taken up by
+      Sharura singh.We cover everthing from Sharura singhincluind javascript
+      and React .js,HTML.
+      </p>
+   </div>
+);
+
+}
+export default Node;
